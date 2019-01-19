dfde9fba0fc5be34fb4aa91e0702ab27781ad9b7
Creig Fortune <fortunephiri004@gmail.com>
2019-01-18 23:04:03 -0500

    update to package.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path')
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{console.log(`server started. port: ${port}`)});
 
