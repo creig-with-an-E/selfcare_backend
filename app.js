21052436d671e0dcb389a712a49eea85a663ca37
Creig Fortune <fortunephiri004@gmail.com>
2019-01-18 23:21:18 -0500

    update to app.js for testing purposes

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 3000;
 app.listen(port,()=>{console.log(`server started. port: ${port}`)});
 
 //test route
-app.get('/',(req, res)=>{res.send('Hello SelfCare')});
+app.get('/',(req, res)=>{res.send('<h1>Self Care the app</h1><p>This is a response from the backend (node.js)running from heroku.com </p>')});
 
