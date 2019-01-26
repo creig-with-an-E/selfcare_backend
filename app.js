55d1609425398778ce13995114f1820cdb18fa26
Creig Fortune <fortunephiri004@gmail.com>
2019-01-26 02:39:39 -0500

    requiring project depencies. 'body parse which will be used to recover data from request 'object'

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
 
 app.listen(port,()=>{console.log(`server started. port: ${port}`)});
 
 //test route
-app.get('/',(req, res)=>{res.send('<h1>Self Care the app</h1><p>This is a response from the backend (node.js)running from heroku.com </p>')});
+app.post('/',(req, res)=>{console.log(req.body.email)});
 
