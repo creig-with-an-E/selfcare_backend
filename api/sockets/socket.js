d8d96b083fd63c9989b5f6f6e919946cd9ea7b5b
Creig Fortune <fortunephiri004@gmail.com>
2019-03-23 03:32:23 -0400

    now able to send private message. whenever user connects the users id is stored so to later  link it to the socket id

--- api/sockets/socket.js
+++ api/sockets/socket.js
@@ -1,18 +1,49 @@
 //socket handlers
 //all socket logic is separated from the server
 const socketio = require('socket.io');
+const User = require('./../models/user');
 
+const mobileSockets = {}
+    let count = 0;
 module.exports.listen = function(app){
-    io = socketio.listen(app);
+    
 
+    io = socketio.listen(app);
+    
     io.on("connection",(socket)=>{
-        console.log("connected");
+
+        socket.on('newUser',credentials=>{
+          const {firstName,password} = credentials;
+
+            mobileSockets[firstName] = socket.id;
+            socket.emit('userCreated',{user:firstName})
+            socket.broadcast.emit('newUser',firstName)
+        })    
+
+        socket.on('chat',users=>{
+            //get prior messages
+            // Conversations.findOrCreate
+
+        })
+
+        socket.on('message',(data)=>{
+           //save message to DB
+           console.log(mobileSockets)
+           socket.emit("incomingMessage",data.text);
+           const receiverSocketId = mobileSockets[data.receiver];
+           socket.to(receiverSocketId).emit('incomingMessage',data.text)})
 
         socket.on("new_message",(data)=>{
             console.log(data)
             socket.emit("new_message",data)
         });
+
+        socket.on("disconnecting",()=>{
+            count--;
+            io.sockets.emit("initialize",{userId:count,connectedUsers:count})
+        })
     });
+
 
     return io
 };
