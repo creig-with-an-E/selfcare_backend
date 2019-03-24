bf81791126c42e9f190483fdd6fc71ca22ef9b72
Sherwayne Walrond <sherwaynewalrond@gmail.com>
2019-03-24 03:24:03 -0400

    initial commit - Schemas and Resolvers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+
 const userRoutes = require('./api/routes/users');
 const professionalRoutes = require('./api/routes/professionals');
 
@@ -19,7 +21,38 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/users', userRoutes);  //users route
 app.use('/professionals', professionalRoutes);  //professional route
+app.use('/graphql', graphqlHttp({
+    // points to a valid graphql schema
+    schema: buildSchema(`
+    type RootQuery{
+        events: [String!]!
+    }
+    
+    type RootMutation{
+        crateEvent(name: String): String
+    }
+    
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    
+    }
+    
+    `),
+    rootValue: {
+        events: () => {
+            return ['cooking', 'eating', 'coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },// points to a js object that has all the resolver functions in it. Resolver functions need to match schema endpoints by name.
+graphiql: true
 
+
+
+}));
 
 
 
